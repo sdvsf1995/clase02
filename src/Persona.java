99ba60b01e2b14686dd48c0faa29eba54095c018
sebas <sebas@sebas-PC>
2018-05-02 20:48:42 -0500

    intentar de nuevo

---
+++ src/Persona.java
@@ -0,0 +1,104 @@
+import java.sql.*;
+import conexion.Conexion;
+import javax.swing.JOptionPane;
+import org.omg.CORBA.ORB;
+public class Persona extends PersonaApp.PersonaPOA {
+    
+    private ORB orb;
+    Conexion conex = new Conexion();
+    @Override
+    public boolean insertarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
+       
+        boolean resultado = false;
+        try {
+            
+            
+            
+            
+            
+            String query = "insert into persona (id ,nombre, apelllido, telefono, direccion, identificacion)" + "values ("+id+" "+nombre+","+apellido+","+telefono+","+direccion+","+identificacion+")";
+            
+            conex.conexion();
+            try (Statement st = conex.conex.createStatement()) {
+                int valor = st.executeUpdate(query);
+                if (valor > 0) {
+                    resultado =true;
+                }
+                //cerramos los recursos.
+            }
+                conex.conex.close();          
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ocurrio un error"+e.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public String consultarPersona(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarPersona(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizarPersona(int id, String nombre, String apellido, String telefono, String direccion, int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+
+    /**
+     *
+     * @return
+     */
+    @Override
+          
+        public String listarPersonas() {
+        
+       String resultado = "";
+       
+        try {
+            String query = "select * from persona" ;
+            conex.conexion();
+            Statement st = conex.conex.createStatement();
+            ResultSet rs = st.executeQuery(query);    
+            while (rs.next()) {
+                resultado += rs.getLong(2)+ " - "
+                        + rs.getString(3)+ " - "
+                         +  rs.getString(4)+ " - "
+                         + rs.getString(5)+ " - "
+                         + rs.getString(6)+ " - "
+                         + rs.getString(7);  
+                
+                                 
+                              
+                    
+                
+                
+                
+                        
+                
+            }
+            st.close();
+            rs.close();
+            conex.conex.close();
+            
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ocurrio un error"+e.getMessage());
+            
+        }
+     
+       return resultado ;
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    }
+    
+
+   
